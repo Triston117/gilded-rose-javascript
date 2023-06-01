6f8f8ab8e121a5c91f11e421dd323051b074053b
Triston117 <roviiknaris@yahoo.com>
2023-06-01 14:36:22 -0700

    Added more test cases

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "vitest";
+import { expect, describe, it, test } from "vitest";
 import { Item, items, updateQuality } from "./gilded-rose.js";
 
 describe("updateQuality", () => {
@@ -7,8 +7,30 @@ describe("updateQuality", () => {
     items.push(testItem);
 
     updateQuality();
-
-    expect(testItem.quality).toBe(2);
     expect(testItem.sellIn).toBe(4);
+    expect(testItem.quality).toBe(2);
+  });
+
+  it("reduces quality by 2 for items with sellIn < 0", () => {
+    const testItem = new Item("basic", -2, 8);
+    items.push(testItem);
+
+    updateQuality();
+    expect(testItem.quality).toBe(6);
+    expect(testItem.sellIn).toBe(-3);
+  });
+
+  it("does not reduce quality to a negative number", () => {
+    const testItem = new Item("basic", 2, 0);
+    items.push(testItem);
+
+    updateQuality();
+    expect(testItem.quality).toBe(0);
+  });
+  it("it increases in quality of 'Aged Brie' items", () => {
+    const testItem = new Item("Aged Brie", 3, 8);
+    items.push(testItem);
+    updateQuality();
+    expect(testItem.quality).toBe(9);
   });
 });
